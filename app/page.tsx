29260eff11834e8ab1649c9b20a4d012af954dc3
J4EH00N <wogns7930@naver.com>
2025-11-26 10:59:55 +0900

    v0.0.1

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Click me</Button>
-    </div>
-  )
-}
